35e1ed9f251df48f12f780c3e5a6db524edd4b49
Amyrosein <Amyroseintz@gmail.com>
2023-06-01 04:09:25 +0330

    migrations, template and Retrieve some Data

--- todos/models.py
+++ todos/models.py
@@ -6,4 +6,5 @@ class Todo(models.Model):
 	created_at = models.DateTimeField(auto_now_add=True)
 	updated_at = models.DateTimeField(auto_now=True)
 
-
+	def __str__(self):
+		return f"Todo number {self.id}"
--- todos/views.py
+++ todos/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Todo
 
-# Create your views here.
+def todo_view(request):
+	todos = Todo.objects.all()
+	context = {"todos": todos}
+	return render(request, "todos/todos.html", context=context)
